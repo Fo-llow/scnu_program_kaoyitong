3773b5b5736718200e4e6c25e6cecbff0bf9b470
Fo-llow <1557605753@qq.com>
2023-03-24 23:24:08 +0800

    Initial commit

---
+++ front/untitled2/src/common.js
@@ -0,0 +1,8 @@
+// 全局变量和方法 https://blog.csdn.net/qq_34663267/article/details/99432029
+// 全局变量
+const baseURL = "http://8.134.142.145:8080/kaoyitong/"
+//const baseURL = "https://orall.top/spring_new_war"
+
+export default {
+    baseURL
+}
